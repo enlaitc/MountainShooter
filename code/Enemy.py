0af65001dc0358191f8b06c26789fc08bb35fd7c
Enlai tc <tccdevop@gmail.com>
2024-11-03 19:27:06 -0300

    trabalho prático

--- code/Enemy.py
+++ code/Enemy.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY
+from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY, WIN_HEIGHT
 from code.EnemyShot import EnemyShot
 from code.Entity import Entity
 
@@ -9,9 +9,24 @@ class Enemy(Entity):
     def __init__(self, name: str, position: tuple):
         super().__init__(name, position)
         self.shot_delay = ENTITY_SHOT_DELAY[self.name]
-
+        self.direction = -1  # -1 para cima, 1 para baixo
+        self.ySpeed = 1  # Velocidade no eixo y
     def move(self):
-        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.name == 'Enemy3':
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+            self.rect.centery += self.ySpeed * self.direction
+
+            # Verificar limites da tela
+            if self.rect.centery <= 0:  # Se bater no topo
+                self.rect.centery = 0
+                self.direction = 1  # Mudar direção para baixo
+                self.ySpeed += 1 # Aumenta a velocidade quando está caindo
+            elif self.rect.centery >= WIN_HEIGHT:  # Se bater na borda inferior
+                self.rect.centery = WIN_HEIGHT
+                self.direction = -1  # Mudar direção para cima
+                self.ySpeed -= 1 # Diminui a velocidade quando está subindo
+        else:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
 
     def shoot(self):
         self.shot_delay -= 1
--- code/Menu.py
+++ code/Menu.py
@@ -23,6 +23,8 @@ class Menu:
             self.window.blit(source=self.surf, dest=self.rect)
             self.menu_text(50, "Mountain", C_ORANGE, ((WIN_WIDTH / 2), 70))
             self.menu_text(50, "Shooter", C_ORANGE, ((WIN_WIDTH / 2), 120))
+            self.menu_text(15, "Thiago Costa Correia", C_WHITE, (100, 10))
+            self.menu_text(15, "RU: 4524910", C_WHITE, (100, 25))
 
             for i in range(len(MENU_OPTION)):
                 if i == menu_option:
